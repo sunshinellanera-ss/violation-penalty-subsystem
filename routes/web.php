737fd831d0748ca236ec0bd73b6e956d552617b2
sunshinellanera-ss <sunshine.llanera@hercorcollege.edu.ph>
2026-02-26 02:12:04 +0800

    Delete routes directory

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-use App\Http\Controllers\StudentController;
-
-Route::get('/students', [StudentController::class, 'index']);
-Route::get('/students/view/{id}', [StudentController::class, 'view']);
-Route::get('/students/edit/{id}', [StudentController::class, 'edit']);
-Route::post('/students/update/{id}', [StudentController::class, 'update']);
-Route::delete('/students/delete/{id}', [StudentController::class, 'destroy']);
